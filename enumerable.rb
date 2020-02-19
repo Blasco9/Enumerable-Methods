2003ce9046620d342856b340440761dac5a65e46
Julian Blasco <blascoblasco9@gmail.com>
2020-02-19 20:19:18 -0300

    Create my_select method

--- enumerable.rb
+++ enumerable.rb
@@ -9,10 +9,20 @@ module Enumerable
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
     i = 0
-    while i < self.size
-      yield(self[i], i)
+    arr = self.to_a
+    while i < arr.size
+      yield(arr[i], i)
       i += 1
     end
   end
-end
 
+  def my_select
+    return to_enum(:my_select) unless block_given?
+    arr = []
+    self.my_each do |el|
+      arr << el if yield(el)
+    end
+    arr = arr.to_h if Hash === self
+    arr
+  end
+end
