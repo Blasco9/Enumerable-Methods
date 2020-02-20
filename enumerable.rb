98e4e240d5e7665a5f41ca5b440be7fcd10767e5
Julian Blasco <blascoblasco9@gmail.com>
2020-02-20 17:12:40 -0300

    Disable CaseEquality, ModuleLength, PerceivedComplexity and CyclomaticComplexity in rubocop

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,5 @@
+# rubocop: disable Style/CaseEquality
+# rubocop: disable Metrics/ModuleLength, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
